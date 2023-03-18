05ad8b8d81cf6a25b67ae19b9052c0fc62f16b48
Taki Enari <taqi.enari@rakuten.com>
2023-03-18 22:22:33 +0900

    Add LocalDateTime Util Create KanjiDataSource abstraction Fix SQlDelight related bugs Implement SQlDelight source

--- shared/src/commonMain/kotlin/co/jp/arcanium/mykanji/kanji/data/SqlDelightKanjiDataSource.kt
+++ shared/src/commonMain/kotlin/co/jp/arcanium/mykanji/kanji/data/SqlDelightKanjiDataSource.kt
@@ -1,5 +1,60 @@
 package co.jp.arcanium.mykanji.kanji.data
 
+import co.jp.arcanium.mykanji.database.KanjiDatabase
 import co.jp.arcanium.mykanji.kanji.domain.KanjiDataSource
 import co.jp.arcanium.mykanji.kanji.domain.model.Kanji
-import kotlinx.datetime.LocalDateTime
+import co.jp.arcanium.mykanji.kanji.domain.util.DateTimeUtil
+import database.Kanji_table
+import kotlinx.datetime.Instant
+import kotlinx.datetime.toLocalDateTime
+
+class SqlDelightKanjiDataSource(
+    database: KanjiDatabase
+) : KanjiDataSource {
+    private val queries = database.kanjiQueries
+
+    override suspend fun insertKanji(kanji: Kanji) {
+        queries.insertKanji(
+            kanji = kanji.kanji,
+            meaning = kanji.meaning,
+            onyomi = kanji.onyomi,
+            kunyomi = kanji.kunyomi,
+            createdAt = DateTimeUtil.toEpochMillis(kanji.createdAt)
+        )
+    }
+
+    override suspend fun selectAllKanji(): List<Kanji> {
+        return queries.selectAllKanji().executeAsList().map { it.toKanji() }
+    }
+
+    override suspend fun selectKanji(kanji: String): Kanji? {
+        return queries.selectKanji(kanji = kanji).executeAsOneOrNull()?.toKanji()
+    }
+
+    override suspend fun deleteKanji(kanji: Kanji) {
+        queries.deleteKanji(kanji = kanji.kanji)
+    }
+
+    override suspend fun updateKanji(kanji: Kanji) {
+        queries.updateKanji(
+            meaning = kanji.kanji,
+            onyomi = kanji.onyomi,
+            kunyomi = kanji.kunyomi,
+            incorrectCount = kanji.incorrectCount?.toLong() ?: 0,
+            kanji = kanji.kanji
+        )
+    }
+}
+
+fun Kanji_table.toKanji(): Kanji {
+    return Kanji(
+        kanji = this.kanji,
+        meaning = this.meaning,
+        onyomi = this.onyomi,
+        kunyomi = this.kunyomi,
+        incorrectCount = this.incorrectCount.toInt(),
+        createdAt = Instant
+            .fromEpochMilliseconds(createdAt)
+            .toLocalDateTime(kotlinx.datetime.TimeZone.currentSystemDefault())
+    )
+}
--- shared/src/commonMain/kotlin/co/jp/arcanium/mykanji/kanji/domain/KanjiDataSource.kt
+++ shared/src/commonMain/kotlin/co/jp/arcanium/mykanji/kanji/domain/KanjiDataSource.kt
@@ -3,10 +3,9 @@ package co.jp.arcanium.mykanji.kanji.domain
 import co.jp.arcanium.mykanji.kanji.domain.model.Kanji
 
 interface KanjiDataSource {
-    fun insertKanji(kanji: Kanji)
-    fun selectAllKanji(): List<Kanji>
-    fun selectKanji(kanji: String): Kanji?
-    fun selectRecentKanji(limit: Long): List<Kanji>
-    fun deleteKanji(kanji: Kanji)
-    fun updateKanji(kanji: Kanji)
+    suspend fun insertKanji(kanji: Kanji)
+    suspend fun selectAllKanji(): List<Kanji>
+    suspend fun selectKanji(kanji: String): Kanji?
+    suspend fun deleteKanji(kanji: Kanji)
+    suspend fun updateKanji(kanji: Kanji)
 }
