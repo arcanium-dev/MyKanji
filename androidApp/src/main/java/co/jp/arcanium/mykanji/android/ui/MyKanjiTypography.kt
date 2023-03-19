d611b077a5d961e1510e2f7228db80a0db15c824
abstrxtt <bmtaki335@gmail.com>
2023-03-19 23:27:50 +0900

    Add and integrate font resources

--- androidApp/src/main/java/co/jp/arcanium/mykanji/android/ui/MyKanjiTypography.kt
+++ androidApp/src/main/java/co/jp/arcanium/mykanji/android/ui/MyKanjiTypography.kt
@@ -1,17 +1,26 @@
 package co.jp.arcanium.mykanji.android.ui
 
-
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import co.jp.arcanium.mykanji.android.R
+
+val regular = Font(R.font.regular)
+val bold = Font(R.font.bold)
+val medium = Font(R.font.medium)
+val semiBold = Font(R.font.semi_bold)
+val light = Font(R.font.light)
 
 internal fun myKanjiTypography(): Typography {
     return Typography(
+        headlineSmall = TextStyle(
+            fontFamily = FontFamily(bold),
+            fontSize = 28.sp
+        ),
         bodyMedium = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
+            fontFamily = FontFamily(bold),
             fontSize = 16.sp
         )
     )
