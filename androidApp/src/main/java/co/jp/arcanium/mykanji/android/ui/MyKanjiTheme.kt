19d1d8356b9e9196e88314345c33f8da9e382ade
abstrxtt <bmtaki335@gmail.com>
2023-03-20 20:25:42 +0900

    Add Text composables for different text
    TODO -
    Fix ui on revision screen
    Check to see if revision set is empty or not when we click on revision screen
    do the revision flow

--- androidApp/src/main/java/co/jp/arcanium/mykanji/android/ui/MyKanjiTheme.kt
+++ androidApp/src/main/java/co/jp/arcanium/mykanji/android/ui/MyKanjiTheme.kt
@@ -3,6 +3,7 @@ package co.jp.arcanium.mykanji.android.ui
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -12,7 +13,7 @@ fun MyKanjiTheme(
 ) {
     MaterialTheme(
         colorScheme = myKanjiColorScheme(isSystemInDarkMode),
-        typography = myKanjiTypography(),
+        typography = Typography(),
         shapes = Shapes(),
         content = content
     )
--- androidApp/src/main/java/co/jp/arcanium/mykanji/android/ui/MyKanjiTypography.kt
+++ androidApp/src/main/java/co/jp/arcanium/mykanji/android/ui/MyKanjiTypography.kt
@@ -1,9 +1,14 @@
 package co.jp.arcanium.mykanji.android.ui
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import co.jp.arcanium.mykanji.android.R
 
@@ -13,23 +18,102 @@ val medium = Font(R.font.medium)
 val semiBold = Font(R.font.semi_bold)
 val light = Font(R.font.light)
 
-internal fun myKanjiTypography(): Typography {
-    return Typography(
-        displayMedium = TextStyle(
-            fontFamily = FontFamily(bold),
-            fontSize = 48.sp
-        ),
-        headlineSmall = TextStyle(
-            fontFamily = FontFamily(bold),
-            fontSize = 28.sp
-        ),
-        bodyMedium = TextStyle(
-            fontFamily = FontFamily(bold),
-            fontSize = 16.sp
-        ),
-        labelMedium = TextStyle(
-            fontFamily = FontFamily(light),
-            fontSize = 16.sp
-        )
+@Composable
+fun RegularText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int,
+    fontColor: Color = MaterialTheme.colorScheme.onSurface,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontWeight = fontWeight,
+        fontFamily = FontFamily(regular),
+        fontSize = fontSize.sp,
+        color = fontColor,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun BoldText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int,
+    fontColor: Color = MaterialTheme.colorScheme.onSurface,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontFamily = FontFamily(bold),
+        fontWeight = fontWeight,
+        fontSize = fontSize.sp,
+        color = fontColor,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun MediumText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int,
+    fontColor: Color = MaterialTheme.colorScheme.onSurface,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontFamily = FontFamily(medium),
+        fontWeight = fontWeight,
+        fontSize = fontSize.sp,
+        color = fontColor,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun SemiBoldText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: Int,
+    fontColor: Color = MaterialTheme.colorScheme.onSurface,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontFamily = FontFamily(semiBold),
+        fontWeight = fontWeight,
+        fontSize = fontSize.sp,
+        color = fontColor,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun LightText(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontWeight: FontWeight,
+    fontSize: Int,
+    fontColor: Color,
+    textAlign: TextAlign? = null
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontFamily = FontFamily(light),
+        fontWeight = fontWeight,
+        fontSize = fontSize.sp,
+        color = fontColor,
+        textAlign = textAlign
     )
 }
